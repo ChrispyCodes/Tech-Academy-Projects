5646a8f6e1cc1d26e42896992515b6c7f9fb9ce1
Christopher <thetoph@outlook.com>
2018-04-20 13:59:28 -0700

    Purple Rain simulation
    JS practice

---
+++ Purple Rain/drop.js
@@ -0,0 +1,27 @@
+
+
+function Drop() {
+  this.x = random(width);
+  this.y = random(-500,-60);
+  this.z = random(0, 20);
+  this.len = map(this.z, 0, 20, 4, 20);
+  this.yspeed = map(this.z, 0, 20, 4, 10);
+
+  this.fall = function() {
+    this.y = this.y + this.yspeed;
+    var grav = map(this.z, 0, 20, 0, 0.1);
+    this.yspeed = this.yspeed + grav;
+
+    if (this.y > height) {
+      this.y = random(-200, -100);
+      this.yspeed = map(this.z, 0, 20, 4, 10);
+    }
+  }
+
+  this.show = function() {
+    var thick = map(this.z, 0, 20, 1, 3);
+    strokeWeight(thick);
+    stroke(138, 43, 226);
+    line(this.x, this.y, this.x, this.y+this.len);
+  }
+}
---
+++ Purple Rain/sketch.js
@@ -0,0 +1,20 @@
+// Purple Rain
+// (138, 43, 226)
+// (230, 230, 250) // background
+
+var drops = [100];
+
+function setup() {
+  createCanvas(1500, 700);
+  for (var i = 0; i < 500; i++) {
+    drops[i] = new Drop();
+  }
+}
+
+function draw() {
+  background(230, 230, 250)
+  for (var i = 0; i < drops.length; i++){
+    drops[i].fall();
+    drops[i].show();
+  }
+}
