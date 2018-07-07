5e75dca469a40919194153c7f2639bdd0b61e6b5
Christopher <thetoph@outlook.com>
2018-07-06 19:50:21 -0700

    Adding newsletter project

---
+++ MvcPractice/MvcPractice/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using MvcPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Chris";
+            user.LastName = "Gaynor";
+            user.Age = 22;
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
