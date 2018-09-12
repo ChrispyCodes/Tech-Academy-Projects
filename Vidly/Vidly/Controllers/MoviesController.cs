f40bcca02d7eb7e9e7695cd6a2d6e9b3a330924f
Christopher <thetoph@outlook.com>
2018-09-11 19:55:59 -0700

    updating movie database
    displaying movies and their details

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -1,18 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
 
+
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
+
             var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
@@ -36,21 +52,24 @@ namespace Vidly.Controllers
         }
 
         //movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
+       
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movie = _context.Movies.Include(m => m.Genre).ToList();
 
-            return View(movies);
+            return View(movie);
+        }
+
+        public ActionResult Details(int id)
+        {
+
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+
+
+            if (movie == null)
+                return HttpNotFound();
+            return View(movie);
         }
 
         private IEnumerable<Movie> GetMovies()
